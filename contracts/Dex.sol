821c77cdd40451ab6b602ba045946ac6fb88e956
ABDUL GAFFAR <gaffarsk273@gmail.com>
2025-10-30 20:34:20 +0530

    Correct comment formatting in Dex.sol
    Fix formatting of the contract documentation comment.

--- contracts/Dex.sol
+++ contracts/Dex.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.24;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
-/**
+/*
  * @title SimpleDEX
  * @dev Basic AMM DEX using constant product formula (x * y = k)
  */
